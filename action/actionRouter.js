8992b935fbceeb5226597a5963a10ebe42ee027f
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-09 10:34:32 -0600

    delete for actions is finished

--- action/actionRouter.js
+++ action/actionRouter.js
@@ -16,4 +16,20 @@ router.get('/', (req, res) => {
     })
 })
 
-module.export = router;
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  action
+    .remove(id)
+    .then(count => {
+      if(!count){
+        res.status(404).json({ message: "The action with the specified ID does not exist."});
+      } else {
+        res.status(200).json(count);
+      }
+    })
+    .catch( error => {
+      res.status(500).json({message: "The action could not be removed", error})
+    })
+})
+module.exports = router;
