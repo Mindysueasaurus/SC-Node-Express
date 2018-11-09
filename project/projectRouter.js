d5500a916626a7d51a166646a0c057f52efcdaef
Mindysueasaurus <mymindyyeager@gmail.com>
2018-11-09 10:18:55 -0600

    put and delete finished for projects

--- project/projectRouter.js
+++ project/projectRouter.js
@@ -33,4 +33,37 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  project
+    .update(id, changes)
+    .then( project =>{
+      if (!project){
+        res.status(404).json({message: "that project was not found"})
+      } else {
+        res.status(200).json(project)
+      }
+    })
+    .catch( error => {
+      res.status(500).json({ message: "error updating project", error})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  project
+    .remove(id)
+    .then(count => {
+      if(!count){
+        res.status(404).json({ message: "The project with the specified ID does not exist."});
+      } else {
+        res.status(200).json(count);
+      }
+    })
+    .catch( error => {
+      res.status(500).json({message: "The project could not be removed", error})
+    })
+})
+
 module.exports = router;
